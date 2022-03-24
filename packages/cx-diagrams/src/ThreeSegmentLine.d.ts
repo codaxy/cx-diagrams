96ea87f4b94de285a4230357c86a2bacb1a09001
Marko Stijak <marko.stijak@codaxy.com>
2022-03-24 11:33:27 +0100

    Minor fixes

--- packages/cx-diagrams/src/ThreeSegmentLine.d.ts
+++ packages/cx-diagrams/src/ThreeSegmentLine.d.ts
@@ -1,9 +1,9 @@
-import { NumberProp, StringProp, Widget } from 'cx/src/core';
-import { Line, LineProps } from 'cx/svg';
+import { NumberProp, StringProp, Widget } from "cx/src/core";
+import { Line, LineProps } from "cx/svg";
 
 interface ThreeSegmentLineProps extends LineProps {
    startOffset?: NumberProp;
-   direction?: StringProp;;
+   direction?: StringProp;
 }
 
 export class ThreeSegmentLine extends Widget<ThreeSegmentLineProps> {}
--- packages/cx-diagrams/src/TwoSegmentLine.d.ts
+++ packages/cx-diagrams/src/TwoSegmentLine.d.ts
@@ -1,9 +1,9 @@
-import { NumberProp, StringProp, Widget } from 'cx/src/core';
-import { Line, LineProps } from 'cx/svg';
+import { NumberProp, StringProp, Widget } from "cx/src/core";
+import { Line, LineProps } from "cx/svg";
 
 interface TwoSegmentLineProps extends LineProps {
    startOffset?: NumberProp;
-   direction?: StringProp;;
+   direction?: StringProp;
 }
 
 export class TwoSegmentLine extends Widget<TwoSegmentLineProps> {}
