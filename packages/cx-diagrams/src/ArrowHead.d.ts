5b2d56449ee205d1a4ea50d3fb6a1f9f2cc8b448
vedranvucic77 <101552426+vedranvucic77@users.noreply.github.com>
2023-08-22 15:29:08 +0200

    Add types

--- packages/cx-diagrams/src/ArrowHead.d.ts
+++ packages/cx-diagrams/src/ArrowHead.d.ts
@@ -1,8 +1,12 @@
-import { Widget } from "cx/src/core";
+import { NumberProp, StringProp, Widget } from "cx/src/core";
 import { NodeProps } from "./Node";
 
 export interface ArrowHeadProps extends NodeProps {
-   position?: "start" | "middle" | "end";
+   position: "start" | "middle" | "end" | StringProp;
+   type: "triangle" | "opened" | "default" | StringProp;
+   size?: NumberProp;
+   fill?: StringProp;
+   stroke?: StringProp;
 }
 
 export class ArrowHead extends Widget<ArrowHeadProps> {}
