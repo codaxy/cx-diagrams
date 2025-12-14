e5ada6583dfbee7795c7692423b78bf842430077
Marko Stijak <mstijak@gmail.com>
2025-12-14 16:49:22 +0100

    Optimize js

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -1,50 +1,60 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import cxjs from './src/integrations/cxjs';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import cxjs from "./src/integrations/cxjs";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [cxjs(), react(), mdx(), tailwind()],
-    prefetch: {
-        defaultStrategy: 'hover',
+  integrations: [cxjs(), react(), mdx(), tailwind()],
+  prefetch: {
+    defaultStrategy: "hover",
+  },
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark-dimmed",
     },
-    markdown: {
-        shikiConfig: {
-            theme: 'github-dark-dimmed',
-        },
+  },
+  vite: {
+    esbuild: {
+      loader: "tsx",
+      include: /.*\.(js|tsx)$/,
+      jsx: "automatic",
+      jsxImportSource: "cx",
     },
-    vite: {
-        esbuild: {
-            loader: 'tsx',
-            include: /.*\.(js|tsx)$/,
-            jsx: 'automatic',
-            jsxImportSource: 'cx',
+    optimizeDeps: {
+      include: ["route-parser", "cx-react"],
+      exclude: ["cx", "cx-diagrams"],
+      esbuildOptions: {
+        loader: {
+          ".js": "jsx",
+          ".tsx": "tsx",
         },
-        optimizeDeps: {
-            include: ['route-parser', 'cx-react'],
-            exclude: ['cx', 'cx-diagrams'],
-            esbuildOptions: {
-                loader: {
-                    '.js': 'jsx',
-                    '.tsx': 'tsx',
-                },
-                jsx: 'automatic',
-                jsxImportSource: 'cx',
-            },
-        },
-        ssr: {
-            external: ['route-parser'],
-        },
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    api: 'modern-compiler',
-                    silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin', 'slash-div'],
-                },
-            },
-        },
+        jsx: "automatic",
+        jsxImportSource: "cx",
+      },
     },
+    ssr: {
+      external: ["route-parser"],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+          silenceDeprecations: [
+            "legacy-js-api",
+            "import",
+            "global-builtin",
+            "slash-div",
+          ],
+        },
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: { manualChunks: undefined }, // merge all JS/CSS
+      },
+    },
+  },
 });
