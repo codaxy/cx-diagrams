58d351c725abc1690e272aff3d40a3911e0feb1f
Marko Stijak <mstijak@gmail.com>
2026-02-22 20:51:09 +0100

    Tweak the example

--- documentation/src/diagrams/DraggableExample.tsx
+++ documentation/src/diagrams/DraggableExample.tsx
@@ -7,7 +7,7 @@ export default () => (
   <cx>
     <Svg class="w-full h-full min-h-[400px] min-w-[600px] bg-white border-t border-b">
       <Diagram center showGrid>
-        <Flow direction="down" gap={2}>
+        <Flow direction="down" gap={2} align="center">
           <Draggable
             offsetX={bind("$page.cell1.offsetX", 0)}
             offsetY={bind("$page.cell1.offsetY", 0)}
@@ -30,27 +30,27 @@ export default () => (
               />
             </Cell>
           </Draggable>
+          <Draggable
+            offsetX={bind("$page.group.offsetX", 0)}
+            offsetY={bind("$page.group.offsetY", 0)}
+          >
+            <Flow direction="right" gap={1} p={0.5}>
+              <Rectangle class="fill-gray-100 stroke-gray-300" />
+              <Cell width={3}>
+                <Shape
+                  text="Group A"
+                  shapeClass="fill-orange-200 stroke-orange-600"
+                />
+              </Cell>
+              <Cell width={3}>
+                <Shape
+                  text="Group B"
+                  shapeClass="fill-orange-200 stroke-orange-600"
+                />
+              </Cell>
+            </Flow>
+          </Draggable>
         </Flow>
-        <Draggable
-          offsetX={bind("$page.group.offsetX", 0)}
-          offsetY={bind("$page.group.offsetY", 0)}
-        >
-          <Flow direction="right" gap={1} mt={3} p={0.5}>
-            <Rectangle class="fill-gray-100 stroke-gray-300" />
-            <Cell width={3}>
-              <Shape
-                text="Group A"
-                shapeClass="fill-orange-200 stroke-orange-600"
-              />
-            </Cell>
-            <Cell width={3}>
-              <Shape
-                text="Group B"
-                shapeClass="fill-orange-200 stroke-orange-600"
-              />
-            </Cell>
-          </Flow>
-        </Draggable>
       </Diagram>
     </Svg>
   </cx>
